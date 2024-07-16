c3e2c67cd6d4d5f209e08aa3a458703a2cca030c
aecamilleri <sanagp85@gmail.com>
2024-07-16 11:21:57 -0300

    SweetAlert2

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     const login = prompt("Ingrese su login:");
     const password = prompt("Ingrese su password:");
 
@@ -8,4 +8,57 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("Acceso denegado. Redirigiendo al inicio...");
         window.location.href = "index.html";
     }
+});
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    async function getCredentials() {
+        const { value: login } = await Swal.fire({
+            title: 'Ingrese su login',
+            input: 'text',
+            inputPlaceholder: 'Login',
+            showCancelButton: true,
+            confirmButtonText: 'Siguiente',
+            cancelButtonText: 'Cancelar'
+        });
+
+        if (!login) {
+            Swal.fire('Acceso cancelado', '', 'info');
+            return;
+        }
+
+        const { value: password } = await Swal.fire({
+            title: 'Ingrese su password',
+            input: 'password',
+            inputPlaceholder: 'Password',
+            showCancelButton: true,
+            confirmButtonText: 'Ingresar',
+            cancelButtonText: 'Cancelar'
+        });
+
+        if (!password) {
+            Swal.fire('Acceso cancelado', '', 'info');
+            return;
+        }
+
+        if (login === "Rosana" && password === "123465") {
+            Swal.fire({
+                title: 'Bienvenida',
+                text: 'Bienvenida, administradora',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+        } else {
+            Swal.fire({
+                title: 'Acceso Denegado',
+                text: 'Acceso denegado. Redirigiendo al inicio...',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                window.location.href = "index.html";
+            });
+        }
+    }
+
+    getCredentials();
 });
